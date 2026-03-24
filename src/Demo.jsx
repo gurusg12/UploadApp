080645c0db0630236dd6bef80270108e018961c4
surajnimondige-max <surajnimondige@gmail.com>
2026-03-24 11:46:08 +0530

    demo

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,21 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const Demo = () => {
-  const[data , setdata] = useState([])
-  const [inputs , setinputs] = useState({title : "" , desc : "" , info : []})
-
-  const handle =  (e)=>{
-    const File = e.target.files[0]
-    const info = {file : File , preview : URL.createObjectURL(File)}
-    setinputs((prev)=>{
-      return{
-      ...prev , info:[...prev.info,info]
-    }})
-  }
   return (
-    <div>
-      
-    </div>
+    <div>Demo</div>
   )
 }
 
